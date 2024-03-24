1f8566f3fc97765a8d2dfd3363b4d3a704381390
austinbeckstead <austinbeckstead314@gmail.com>
2024-03-24 01:34:18 -0600

    Created Database.js

---
+++ database.js
@@ -0,0 +1,77 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('users');
+const recipeCollection = db.collection('recipes');
+const userRecipeCollection = db.collection('userRecipes');
+
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+function addRecipe(recipe) {
+  recipeCollection.insertOne(recipe);
+}
+
+function getRecipes() {
+  const cursor = recipeCollection.find();
+  return cursor.toArray();
+}
+
+async function addUserRecipe(username, newRecipes) {
+        const result = await db.collection('userRecipes').updateOne(
+            { username: username },
+            { $set: { recipes: newRecipes } },
+            { upsert: true }
+          );
+      return result;
+  }
+
+  function getUserRecipes(username) {
+      const cursor = db.collection('userRecipes').find({ username: username });
+      const userRecipes = cursor.toArray();
+      return userRecipes;
+  }
+  
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addRecipe,
+  getRecipes,
+  addUserRecipe,
+  getUserRecipes,
+};
