71526a9502de74de76599b1dcd7d55aadeabe0f0
austinbeckstead <austinbeckstead314@gmail.com>
2024-03-24 02:04:42 -0600

    implemented endpoints (not working yet)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -13,6 +17,61 @@ app.use(express.static('public'));
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.username, req.body.password);
+
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+  console.log("Logging in");
+  const user = await DB.getUser(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+apiRouter.get('/user/:username', async (req, res) => {
+  const user = await DB.getUser(req.params.username);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ username: user.username, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
 
 // GetRecipes
 apiRouter.get('/recipes', (_req, res) => {
@@ -47,14 +106,22 @@ app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 // addRecipe stores a new recipe for as long as the service is running 
-let recipes = [];
+/*let recipes = [];
 function addRecipe(newRecipe, recipes) {
     recipes.push(newRecipe);
     return recipes;
 }
-
+*/
--- public/login.js
+++ public/login.js
@@ -1,4 +1,4 @@
-function login() {
+/*function login() {
     localStorage.clear();
     const name = document.querySelector("#username");
     const password = document.querySelector("#password");
@@ -7,3 +7,34 @@ function login() {
     localStorage.setItem("userRecipes", JSON.stringify([]));
     window.location.href = "home.html";
   }
+*/
+
+function login(){
+  console.log("HE");
+  loginOrCreate(`/auth/login`);
+}
+async function create() {
+  loginOrCreate(`/auth/create`);
+}
+async function loginOrCreate(endpoint) {
+  const username = document.querySelector('#username')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    body: JSON.stringify({ username: username, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  if (response.ok) {
+    localStorage.setItem('username', username);
+    window.location.href = 'home.html';
+  } else {
+    const body = await response.json();
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
